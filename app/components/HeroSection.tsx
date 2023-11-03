791201b3455598f21ceab1f679b99678ff171dac
camchu2001 <chuphuongcam@gmail.com>
2023-11-02 20:59:14 -0500

    chore: initial landing page

---
+++ app/components/HeroSection.tsx
@@ -0,0 +1,7 @@
+export const HeroSection = () => {
+  return (
+    <section>
+        <h1 className="text-white">Hello, I am Cam Chu</h1>
+    </section>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,9 @@
-import Image from 'next/image'
-import Link from 'next/link'
+import { HeroSection } from './components'
 
 export default async function Home() {
   return (
-    <div>
-      <h1 className="text-7xl font-extrabold">Hello I&apos;m
-        <span className="bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent"> Cam!</span>
-      </h1>
-      <p className="mt-3 text-xl text-gray-600">Welcome to my site! Check out my projects!</p>
-      <h2 className="mt-24 font-bold text-gray-700 text-3xl">My Projects</h2>
-    </div>
+    <main className='flex min-h-screen flex-col bg-black container mx-auto'>
+      <HeroSection/>
+    </main>
   )
-
 }
