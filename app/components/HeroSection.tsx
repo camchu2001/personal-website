cd39431b828fcb345e013a9b72c2c54cf6817a31
camchu2001 <chuphuongcam@gmail.com>
2023-11-15 23:13:52 -0600

    chore: add react-type-animation

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -1,5 +1,7 @@
+"use client";
 import React from 'react';
 import Image from 'next/image';
+import {TypeAnimation} from 'react-type-animation'
 
 export const HeroSection = () => {
   return (
@@ -8,25 +10,39 @@ export const HeroSection = () => {
        {/* WELCOME TEXT */}
         <div className="sm:col-span-6 lg:col-span-7.5 place-self-center text-center sm:text-left justify-self-start">
           <h1 className="text-[#FFC4A9] mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
-                    I'm Cam Chu
+            Hi, I'm 
+            <TypeAnimation
+              sequence={[
+                " Cam Chu",
+                1200,
+                " a Web Developer",
+                1200
+              ]}
+              wrapper="span"
+              speed={50}
+              repeat={Infinity}
+            />
           </h1>
+          <br></br>
           <p className='text-[#FFC57C] text-base sm:text-lg mb-6 lg:text-xl font-mono'>
-                    Hello, welcome to my website! <br></br>
+                    Welcome to my website 🚀 <br></br>
                     I am a software engineer on a mission to learn and create.
                     Feel free to explore my projects and check out my coding journey.
           </p>
+          <br></br>
           <div className="">
-            <button className='px-6 py-3 w-full sm:w-fit sm:mb-2 rounded-full mr-4 mb-3 text-white
-                              bg-gradient-to-r from-rose-400 to-orange-300'>
+            <button className='px-6 py-3 w-full sm:w-fit sm:mb-2 rounded-full mr-4 mb-3 font-semibold
+                               bg-gradient-to-r from-rose-400 to-orange-300  text-white border border-white
+                               hover:font-bold hover:from-pink-500 hover:via-red-500 hover:to-yellow-500 focus:outline-none'>
              My Resume
             </button>
-            <button className='px-6 py-3 w-full sm:w-fit sm:mb-2 rounded-full bg-transparent
-                               hover:bg-slate-800 text-white border border-white'>
+            <button className='px-6 py-3 w-full sm:w-fit sm:mb-2 rounded-full font-semibold
+                             bg-[#6EA172] text-white border border-white 
+                             hover:bg-[#3c5d3f] hover:font-bold focus:outline-none'>
               Contact Me
             </button> 
           </div> 
         </div>
-
         <div className="sm:col-span-2 lg:col-span-2"></div>
 
          {/* IMAGE */}
