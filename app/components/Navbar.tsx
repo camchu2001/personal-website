8dfca42bc1f1a132312ac8068999bb003a352888
camchu2001 <chuphuongcam@gmail.com>
2023-11-17 00:42:57 -0600

    chore: adjust responsiveness for pc

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -12,65 +12,60 @@ export const Navbar = () => {
     return (
         <nav className='fixed mx-auto top-0 left-0 right-0 z-10 bg-[#2F4858] bg-opacity-100 backdrop-blur-[0.5rem]'>
             <div className="flex container lg:py-4 flex-wrap items-center justify-between mx-auto px-4 py-2">
-                <div className="grid grid-cols-12">
-                    {/* first line */}
-                    <div className="col-span-12 mt-2">
-                        <div className="grid grid-cols-12">
-                            <Link href={"/"} className='col-span-2'>
-                                <Image src={Home} alt='Homepage' width={70} height={50} className='ml-2 md:w-[100px]' />
+                {/* first line */}
+                <div className="mt-2">
+                    <div className="grid grid-cols-12">
+                        <Link href={"/"} className='col-span-2'>
+                            <Image src={Home} alt='Homepage' width={70} height={50} className='ml-2 md:w-[100px]' />
+                        </Link>
+
+                        <div className="col-span-8"></div>
+
+                        <div className="mobile-menu block mt-3 md:hidden col-span-2">
+                            {!navbarOpen ? (
+                                <button
+                                    onClick={() => setNavbarOpen(true)}
+                                    className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white"
+                                >
+                                    <Bars3Icon className="h-5 w-5" />
+                                </button>
+                            ) : (
+                                <button
+                                    onClick={() => setNavbarOpen(false)}
+                                    className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white"
+                                >
+                                    <XMarkIcon className="h-5 w-5" />
+                                </button>
+                            )}
+                        </div>
+                    </div>
+
+                </div>
+
+                {/* second line */}
+                <div className={`menu ${navbarOpen ? 'block' : 'hidden'} md:block md:w-auto col-span-10 flex justify-center`} id='navbar'>
+                    <ul className="flex items-center p-4 md:p-0 md:flex-row md:space-x-8 mt-0">
+                        <li>
+                            <Link href={"#projects"} className='block py-2 md:pl-3 pr-4 text-[#FFC57C] font-semibold md:text-2xl text-xl rounded md:p-0 hover:text-white '>
+                                Projects
                             </Link>
-
-                            <div className="col-span-8"></div>
-
-                            <div className="mobile-menu block mt-3 md:hidden col-span-2">
-                                {!navbarOpen ? (
-                                    <button
-                                        onClick={() => setNavbarOpen(true)}
-                                        className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white"
-                                    >
-                                        <Bars3Icon className="h-5 w-5" />
-                                    </button>
-                                ) : (
-                                    <button
-                                        onClick={() => setNavbarOpen(false)}
-                                        className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white"
-                                    >
-                                        <XMarkIcon className="h-5 w-5" />
-                                    </button>
-                                )}
-                            </div>
-                        </div>
-
-                    </div>
-
-                    {/* second line */}
-                    <div className="col-span-12">
-
-                        <div className={`menu ${navbarOpen ? 'block' : 'hidden'} md:block md:w-auto col-span-10 flex justify-center`} id='navbar'>
-                            <ul className="flex items-center p-4 md:p-0 md:flex-row md:space-x-8 mt-0">
-                                <li>
-                                    <Link href={"#projects"} className='block py-2 md:pl-3 pr-4 text-[#FFC57C] font-semibold md:text-2xl text-xl rounded md:p-0 hover:text-white '>
-                                        Projects
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link href={"#experience"} className='block py-2 md:pl-3 pr-4 text-[#FFC57C] font-semibold md:text-2xl text-xl rounded md:p-0 hover:text-white '>
-                                        Experience
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link href="https://github.com/camchu2001">
-                                        <Image src={GithubIcon} alt="Github Icon" className='ml-0' />
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link href="https://www.linkedin.com/in/cam-chu/">
-                                        <Image src={LinkedinIcon} alt="Linkedin Icon" className='ml-4' />
-                                    </Link>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
+                        </li>
+                        <li>
+                            <Link href={"#experience"} className='block py-2 md:pl-3 pr-4 text-[#FFC57C] font-semibold md:text-2xl text-xl rounded md:p-0 hover:text-white '>
+                                Experience
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="https://github.com/camchu2001">
+                                <Image src={GithubIcon} alt="Github Icon" className='ml-0' />
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="https://www.linkedin.com/in/cam-chu/">
+                                <Image src={LinkedinIcon} alt="Linkedin Icon" className='ml-4' />
+                            </Link>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </nav >
