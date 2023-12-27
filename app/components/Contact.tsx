9547e1b24e4ce4c5aea190f69c5020de2a8bfded
camchu2001 <chuphuongcam@gmail.com>
2023-12-26 18:15:37 -0600

    chore: modify favicon

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FaPaperPlane } from 'react-icons/fa';
+// import { FaPaperPlane } from 'react-icons/fa';
 import Image from 'next/image';
 
 export const Contact = () => {
