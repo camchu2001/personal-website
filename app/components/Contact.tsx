d73cbbc319cbe3c0c237ba839b4addf002aa5d44
camchu2001 <chuphuongcam@gmail.com>
2023-12-26 19:10:58 -0600

    fix: fix text color and submit button for contact section

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -15,19 +15,20 @@ export const Contact = () => {
         </p>
       </div>
 
-      <form className="mt-6 flex flex-wrap flex-col">
+      <form className="mt-6 flex flex-wrap flex-col md:items-center">
         <input type="email" name='senderEmail' placeholder='Your email ' maxLength={500} required 
-          className='p-2 mb-4 rounded-md border-black'/> 
+          className='p-2 mb-4 h-[3rem] md:w-[700px] rounded-md border-black text-black'/> 
 
         <input type="text" name='emailSubject' placeholder='Your subject ' 
-          className='p-2 mb-4 rounded-md border-black'/>
+          className='p-2 mb-4 h-[3rem] md:w-[700px] rounded-md border-black text-black'/>
 
         <textarea name="message" placeholder="Your message " maxLength={5000} required
-          className='p-2 mb-4 h-[px] rounded-md border-black border-shadow'>
+          className='p-2 mb-4 h-[10rem] md:w-[700px] rounded-md border-black text-black'>
         </textarea>
 
         <div className="flex flex-wrap flex-col items-center">
-          <button type="submit" className='rounded-lg bg-black p-2 w-[100px] flex items-center'>
+          <button type="submit" 
+            className='rounded-lg bg-black p-2 w-[100px] flex items-center'>
           Submit <FaPaperPlane className="ml-2"/>
           </button>
         </div>
