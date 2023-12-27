527618e48672dfcbb2e095b136acffffe7abffad
camchu2001 <chuphuongcam@gmail.com>
2023-12-26 18:14:35 -0600

    feat: contact section

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -1,15 +1,26 @@
 import React from 'react';
+import { FaPaperPlane } from 'react-icons/fa';
+import Image from 'next/image';
 
 export const Contact = () => {
   return (
-    <section id="contact">
-      <div className="mt-16">
-        <h1 className="text-center text-xl font-bold ">Contact Me</h1>
-        <p className="text-center">Feel free to reach out at <a 
-          href="mailto:camchu.dev.@gmail.com" className="underline">
-			camchu.dev.@gmail.com
-        </a> or use the form below!
+    <section id="contact" className='mt-28'>
+      <div className="flex flex-wrap justify-center">
+        <Image src={'/contact.png'} alt={'contact'} width={70} height={70}
+          className='mb-10'/>
+        <p className="text-center text-base sm:text-lg lg:text-xl font-mono">
+          Feel to reach out using the form below or send me an email at <a 
+            href="mailto:camchu.dev.@gmail.com" className="underline">camchu.dev.@gmail.com
+          </a> 🌟
         </p>
+      </div>
+
+      <div className="mt-8">
+        {/* <input type="email"/>
+        <textarea name="" id="" cols="30" rows="10"></textarea>
+        <button type="submit">Submit
+          <FaPaperPlane />
+        </button> */}
       </div>
     </section>
   );
