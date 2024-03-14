fbe62775e3d1c0fe2eb5124ee302abf33189828f
camchu2001 <chuphuongcam@gmail.com>
2024-03-14 10:55:44 -0500

    chore: modify color for dark theme

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const Footer = () => {
   return (
-    <footer className="mb-5 px-4 text-center text-gray-500 bg-[#2F4858]">
+    <footer className="mb-5 px-4 text-center text-gray-500 bg-[#253946]">
       <small className='mb-2 block text-xs font-semibold'>
         2024 &copy; Cam Chu
       </small>
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { About, Navbar, Contact, Footer } from './components';
 
 export default async function Home() {
   return (
-    <main className='flex min-h-screen flex-col bg-[#2F4858]'>
+    <main className='flex min-h-screen flex-col bg-[#253946]'>
       <Navbar />
       <div className="container mt-28 sm:mt-32 md:mt-22 mx-auto px-12 py-4">
         <About />
